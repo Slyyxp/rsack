0adeb3f9e0de779a8cbc131dedd237c8ca7a036b
Slyyxp <11709831+Slyyxp@users.noreply.github.com>
2022-02-16 16:28:19 +0000

    Remove examples in favour of README

--- examples/bugs_client.py
+++
@@ -1,8 +0,0 @@
-from rsack.clients import bugs
-
-client = bugs.Client() # Initialize client object
-client.auth(username='', password='') # Authorize user
-
-artist_response = client.get_meta(type='artist', id=80219706) # Make call for artist information using artist UID
-album_response = client.get_meta(type='album', id=4071297) # Make call for album information using album UID
-track_response = client.get_meta(type='track', id=6147328) # Make call for track information using track UID
