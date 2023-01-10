d0fa258e1eb058356935a56d7d5d4b780e349c27
SYQ <57177286+Yuq-q@users.noreply.github.com>
2023-01-10 22:55:32 +0800

    Update kkbox.py
    If you want to download all albums of an artist, only the albums shown in https://play.kkbox.com/artist/XXXXXXXXXXXXXX will be downloaded.
    But this is only a part of this artist's albums, not all of this artist's albums.
    his all albums are showcased at https://play.kkbox.com/artist/DXQVoCQF1asjEu9yaW/albums. and should be downloaded from here
    In addition, you seem to use the 'get_date' function in ' rsack\clients\kkbox.py ' to capture the release time of the album through ' Beautifulsoup'. In fact, the release time of the album can also be obtained from 'get_artist_albums'
    (In addition, I had the log print out the name of the album currently being downloaded)

--- rsack/kkbox.py
+++ rsack/kkbox.py
@@ -17,7 +17,9 @@ class Download():
                             region_bypass=True)
         if url.split("/")[-2] == "artist":
             artist = self.client.get_artist(self.id)
-            for album in artist['album']:
+            artist_raw_id = artist["profile"]["artist_id"]
+            artist_albums = self.client.get_artist_albums(artist_raw_id,20,0)
+            for album in artist_albums:
                 self._download_album(album['encrypted_album_id'])
         else:
             self._download_album(self.id)
@@ -93,6 +95,7 @@ class Download():
     def _download_album(self, id):
         self.meta = self.client.get_album(id=id)
         self.meta['release_date'] = self.client.get_date(id)
+        logger.info("Album Name: " + self.meta['album']['album_name'])
         song_list = self.client.get_album_more(self.meta['album']['album_id'])
         self._create_album_folder()
         self._download_cover()
